9584bad6857590ad8752a015f37adbb303480d57
sandstranger <sandstranger@gmail.com>
2026-03-22 11:02:12 +0400

    always use gles 3.1 for angle working

--- src/window/SDL2Window.cpp
+++ src/window/SDL2Window.cpp
@@ -488,10 +488,10 @@ bool SDL2Window::initialize() {
                 SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, flags);
 
                 SDL_Log(g_useGLES2_0 ? "Legacy OpenGL ES 2.0 is using for rendering" :
-                        "OpenGL ES 3.2 is using for rendering");
+                        "OpenGL ES 3.1 is using for rendering");
                 SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
                 SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, g_useGLES2_0 ? 2 : 3);
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, g_useGLES2_0 ? 0 : 2);
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, g_useGLES2_0 ? 0 : 1);
                 SDL_GL_SetAttribute(SDL_GL_CONTEXT_NO_ERROR, 1);
                 samples = createWindowAndGLContext("OpenGL ES");
 
